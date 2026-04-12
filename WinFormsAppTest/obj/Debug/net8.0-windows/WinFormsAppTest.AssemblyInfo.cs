2bb4f900a33952784a1733feeb16ef4c77f76615
haphcc <phuocha@outlook.com>
2026-04-13 00:13:59 +0700

    cache máy

--- WinFormsAppTest/obj/Debug/net8.0-windows/WinFormsAppTest.AssemblyInfo.cs
+++ WinFormsAppTest/obj/Debug/net8.0-windows/WinFormsAppTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsAppTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+140a4da395502429ab618db785430907f9695ae9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3901b10aba9136d1e4510423abb18c96045bb27")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsAppTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsAppTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
